9b6301714218bf449eaee27892b42700c87bbe10
Mauricio Davila <Mauro@MacBook-Pro-de-Mauricio.local>
2020-04-09 20:09:53 -0300

    Init Clase 6

--- Clase5.1/src/Clase5.1.c
+++ Clase5.1/src/Clase5.1.c
@@ -12,14 +12,20 @@
 #include <stdlib.h>
 #include "utn.h"
 
-#define QTY_EMPLEADOS 3
+
+#define QTY_EMPLEADOS 9
 
 int imprimirArrayInt(int pArray[],int size);
-int ordenar(int pArray[],int size);
+int ordenarOpcionA(int pArray[],int size);
+int ordenarOpcionB(int pArray[],int size);
+int ordenarOpcionC(int pArray[],int size);
 
 int main(void)
 {
-	int arrayEdades[QTY_EMPLEADOS];
+	int arrayEdades[QTY_EMPLEADOS] = {54,26,93,17,77,31,44,55,27};
+	int arrayEdadesA[QTY_EMPLEADOS] = {54,26,93,17,77,31,44,55,27};
+	int arrayEdadesB[QTY_EMPLEADOS] = {54,26,93,17,77,31,44,55,27};
+	int arrayEdadesC[QTY_EMPLEADOS] = {54,26,93,17,77,31,44,55,27};
 	int arraySalarios[QTY_EMPLEADOS];
 	int i;
 	int bufferEdad;
@@ -28,7 +34,7 @@ int main(void)
 	int respuestaS;
 	int respuesta;
 	int opcion;
-
+/*
 	for(i = 0; i < QTY_EMPLEADOS; i++)
 	{
 		respuestaE = utn_getNumero(&bufferEdad, "Edad?\n","Error tiene que ser de 18 a 65\n",18,65,2);
@@ -41,10 +47,11 @@ int main(void)
 	}
 	imprimirArrayInt(arrayEdades,QTY_EMPLEADOS);
 	imprimirArrayInt(arraySalarios,QTY_EMPLEADOS);
+*/
 
 	do
 	{
-		respuesta = utn_getNumero(&opcion, "1-Modificar Edad\n2-Modificar Salario\n3-Mostrar\n4-Ordenar\n6-Salir\n","Error tiene que ser de 1 a 6\n",1,6,2);
+		respuesta = utn_getNumero(&opcion, "1-Modificar Edad\n2-Modificar Salario\n3-Mostrar\n4-Ordenar Op A\n5-Ordenar Op B\n6-Ordenar Op C\n7-Salir\n","Error tiene que ser de 1 a 6\n",1,6,2);
 		if(respuesta == 0)
 		{
 			switch(opcion)
@@ -67,29 +74,57 @@ int main(void)
 					break;
 				case 3:
 					imprimirArrayInt(arrayEdades,QTY_EMPLEADOS);
-					imprimirArrayInt(arraySalarios,QTY_EMPLEADOS);
+					//imprimirArrayInt(arraySalarios,QTY_EMPLEADOS);
 					break;
 				case 4:
-					if(ordenar(arrayEdades,QTY_EMPLEADOS) == 0)
+					imprimirArrayInt(arrayEdadesA,QTY_EMPLEADOS);
+					respuesta = ordenarOpcionA(arrayEdadesA,QTY_EMPLEADOS);
+					if(respuesta > 0)
 					{
-						printf("\nSIIIIIII ordeno");
+						printf("\n\nOpcion A - Cantidad de Iteraciones: %d\n\n",respuesta);
 					}
+					imprimirArrayInt(arrayEdadesA,QTY_EMPLEADOS);
+					break;
+				case 5:
+					imprimirArrayInt(arrayEdadesB,QTY_EMPLEADOS);
+					respuesta = ordenarOpcionB(arrayEdadesB,QTY_EMPLEADOS);
+					if(respuesta > 0)
+					{
+						printf("\n\nOpcion B - Cantidad de Iteraciones: %d\n\n",respuesta);
+					}
+					imprimirArrayInt(arrayEdadesB,QTY_EMPLEADOS);
+					break;
+				case 6:
+					imprimirArrayInt(arrayEdadesC,QTY_EMPLEADOS);
+					respuesta = ordenarOpcionC(arrayEdadesC,QTY_EMPLEADOS);
+					if(respuesta > 0)
+					{
+						printf("\n\nOpcion C - Cantidad de Iteraciones: %d\n\n",respuesta);
+					}
+					imprimirArrayInt(arrayEdadesC,QTY_EMPLEADOS);
 					break;
 
 			}
 		}
-	}while(opcion != 6);
+	}while(opcion != 7);
 
 	return EXIT_SUCCESS;
 }
 
-
-int ordenar(int pArray[],int size)
+/**
+ * \brief Ordena un array de enteros de manera ascendente
+ * \param pArray Es el puntero al array a ser ordenado
+ * \param size Es la longitud del array
+ * \return El numero de iteraciones reralizadas o -1 para indicar un error
+ *
+ */
+int ordenarOpcionA(int pArray[],int size)
 {
 	int flagSwap=1;
 	int i;
 	int buffer;
 	int retorno = -1;
+	int contadorIteraciones=0;
 	if(pArray != NULL && size > 0)
 	{
 		while(flagSwap)
@@ -104,14 +139,100 @@ int ordenar(int pArray[],int size)
 					pArray[i] = pArray[i+1];
 					pArray[i+1] = buffer;
 				}
+				contadorIteraciones++;
 			}
 		}
-		retorno = 0;
+		retorno = contadorIteraciones;
+	}
+	return retorno;
+}
+
+/**
+ * \brief Ordena un array de enteros de manera ascendente
+ * \param pArray Es el puntero al array a ser ordenado
+ * \param size Es la longitud del array
+ * \return El numero de iteraciones reralizadas o -1 para indicar un error
+ *
+ */
+int ordenarOpcionB(int pArray[],int size)
+{
+	int flagSwap=1;
+	int i;
+	int buffer;
+	int retorno = -1;
+	int contadorIteraciones=0;
+	int limite;
+
+	if(pArray != NULL && size > 0)
+	{
+		limite = size-1;
+		while(flagSwap)
+		{
+			flagSwap = 0;
+			for(i = 0 ; i < limite ; i++,limite--)
+			{
+				if(pArray[i] > pArray[i+1])
+				{
+					flagSwap = 1;
+					buffer = pArray[i];
+					pArray[i] = pArray[i+1];
+					pArray[i+1] = buffer;
+				}
+				contadorIteraciones++;
+			}
+
+		}
+		retorno = contadorIteraciones;
+	}
+	return retorno;
+}
+
+/**
+ * \brief Ordena un array de enteros de manera ascendente
+ * \param pArray Es el puntero al array a ser ordenado
+ * \param size Es la longitud del array
+ * \return El numero de iteraciones reralizadas o -1 para indicar un error
+ *
+ */
+int ordenarOpcionC(int pArray[],int size)
+{
+	int flagSwap=1;
+	int i;
+	int j;
+	int buffer;
+	int retorno = -1;
+	int contadorIteraciones=0;
+
+	if(pArray != NULL && size > 0)
+	{
+		for(i = 0 ; i < size-1 ; i++)
+		{
+			for(j = i+1 ; j < size ; j++)
+			{
+				if(pArray[i] > pArray[j])
+				{
+					flagSwap = 1;
+					buffer = pArray[i];
+					pArray[i] = pArray[j];
+					pArray[j] = buffer;
+				}
+				contadorIteraciones++;
+			}
+		}
+		retorno = contadorIteraciones;
 	}
 	return retorno;
 }
 
 
+
+/**
+ * \brief Imprime a modo DEBUG la informacion de un array de enteros
+ * \param pArray Es el puntero al array a ser ordenado
+ * \param size Es la longitud del array
+ * \return 0 si Ok o -1 para indicar un error
+ *
+ */
 int imprimirArrayInt(int pArray[],int size)
 {
 	int i;
---
+++ Clase6/src/Clase6.c
@@ -0,0 +1,155 @@
+/*
+ ============================================================================
+ Name        : Clase6.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#define QTY_EMPLEADOS 9
+
+int printArrayInt(int* pArray, int limite);
+int ordenarArrayInt(int* pArray, int limite);
+int ordenarArrayIntMal(int* pArray, int limite);
+
+int main(void)
+{
+	int arrayEdades[QTY_EMPLEADOS] = {54,26,93,17,77,31,44,55,27};
+	int respuesta;
+
+	printArrayInt(arrayEdades,QTY_EMPLEADOS);
+	respuesta = ordenarArrayInt(arrayEdades,QTY_EMPLEADOS);
+	if( respuesta > 0)
+	{
+		printf("\n\n Iteraciones : %d\n\n",respuesta);
+	}
+	printArrayInt(arrayEdades,QTY_EMPLEADOS);
+
+
+	respuesta = ordenarArrayInt(arrayEdades,QTY_EMPLEADOS);
+	if( respuesta > 0)
+	{
+		printf("\n\n Iteraciones : %d\n\n",respuesta);
+	}
+
+
+	return EXIT_SUCCESS;
+}
+
+/**
+ * \brief Imprime a modo DEBUG la informacion de un array de enteros
+ * \param pArray Es el puntero al array a ser ordenado
+ * \param size Es la longitud del array
+ * \return 0 si Ok o -1 para indicar un error
+ *
+ */
+int printArrayInt(int* pArray, int limite)
+{
+	int retorno = -1;
+	int i;
+	if(pArray != NULL && limite >= 0)
+	{
+		retorno = 0;
+		for(i=0;i<limite;i++)
+		{
+			printf("\n[DEBUG]  Indice: %d - Valor: %d",i,pArray[i]);
+		}
+	}
+	return retorno;
+}
+
+/**
+ * \brief Ordena un array de enteros DESC
+ * \param pArray Es el puntero al array a ser ordenado
+ * \param size Es la longitud del array
+ * \return Cantidad de iteraciones necesarias para ordenar si Ok o -1 para indicar un error
+ *
+ */
+int ordenarArrayInt(int* pArray, int limite)
+{
+	int flagSwap;
+	int i;
+	int contador=0;
+	int retorno = -1;
+	int buffer;
+	int nuevoLimite;
+
+
+	if(pArray != NULL && limite >= 0)
+	{
+		nuevoLimite = limite - 1;
+		do
+		{
+			flagSwap=0;
+			for(i=0;i<nuevoLimite;i++)
+			{
+				contador++;
+				if(pArray[i] < pArray[i+1])
+				{
+					flagSwap = 1;
+					buffer = pArray[i];
+					pArray[i] = pArray[i+1];
+					pArray[i+1] = buffer;
+				}
+			}
+			nuevoLimite--;
+		}while(flagSwap);
+		retorno = contador;
+	}
+	return retorno;
+}
+
+
+
+/**
+ * \brief Ordena un array de enteros DESC MAL
+ * \param pArray Es el puntero al array a ser ordenado
+ * \param size Es la longitud del array
+ * \return Cantidad de iteraciones necesarias para ordenar si Ok o -1 para indicar un error
+ *
+ */
+int ordenarArrayIntMal(int* pArray, int limite)
+{
+	int i;
+	int j;
+	int contador=0;
+	int retorno = -1;
+	int buffer;
+
+
+
+	if(pArray != NULL && limite >= 0)
+	{
+		for(i=0;i<limite -1;i++)
+		{
+			for(j=i+1;j<limite;j++)
+			{
+				contador++;
+				if(pArray[i] < pArray[j])
+				{
+					buffer = pArray[i];
+					pArray[i] = pArray[j];
+					pArray[j] = buffer;
+				}
+
+			}
+		}
+		retorno = contador;
+	}
+	return retorno;
+}
+
+
+
+
+
+
+
+
+
+
+
