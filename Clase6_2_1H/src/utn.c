eb6e8e5460c767fbdc90d6749394a3401478d221
Mauricio Davila <Mauro@MacBook-Pro-de-Mauricio.local>
2020-04-14 22:48:02 -0300

    Clase 6 Segunda parte

--- Clase6_2_1H/src/utn.c
+++ Clase6_2_1H/src/utn.c
@@ -8,6 +8,26 @@ static int getInt(int* pResultado);
 static int myGets(char* cadena, int longitud);
 
 /**
+ * \brief
+ * \param pResultado Puntero al espacio de memoria donde se dejara el resultado de la funcion
+ * \return Retorna 0 (EXITO) si se obtiene un numero entero y -1 (ERROR) si no
+ *
+ */
+static int getInt(int* pResultado)
+{
+    int retorno=-1;
+    char buffer[4096];
+
+    if(myGets(buffer,sizeof(buffer))==0 && esNumerica(buffer))
+    {
+    	retorno = 0;
+    	*pResultado = atoi(buffer);
+    }
+    return retorno;
+}
+
+
+/**
  * \brief 	Lee de stdin hasta que encuentra un '\n' o hasta que haya copiado en cadena
  * 			un máximo de 'longitud - 1' caracteres.
  * \param pResultado Puntero al espacio de memoria donde se copiara la cadena obtenida
@@ -17,22 +37,14 @@ static int myGets(char* cadena, int longitud);
  */
 static int myGets(char* cadena, int longitud)
 {
+	fpurge(stdin);
+	fgets(cadena,longitud,stdin);
 
-	return -1;
+	cadena[strlen(cadena)-1] = '\0';
+
+	return 0;
 }
 
-/**
- * \brief Verifica si la cadena ingresada es numerica
- * \param pResultado Puntero al espacio de memoria donde se dejara el resultado de la funcion
- * \return Retorna 0 (EXITO) si se obtiene un numero entero y -1 (ERROR) si no
- *
- */
-static int getInt(int* pResultado)
-{
-    int retorno=-1;
-
-    return retorno;
-}
 
 /**
  * \brief Verifica si la cadena ingresada es numerica
@@ -43,7 +55,21 @@ static int getInt(int* pResultado)
 static int esNumerica(char* cadena)
 {
 	int retorno = 1;
+	int i=0;
 
+	if(cadena[0] == '-')
+	{
+		i = 1;
+	}
+
+	for( ; cadena[i] != '\0' ; i++)
+	{
+		if(cadena[i] > '9' || cadena[i] < '0')
+		{
+			retorno =  0;
+			break;
+		}
+	}
 	return retorno;
 }
 
@@ -60,7 +86,23 @@ static int esNumerica(char* cadena)
 int utn_getNumero(int* pResultado, char* mensaje, char* mensajeError, int minimo, int maximo, int reintentos)
 {
 	int retorno = -1;
+	int buffer;
 
+	if(pResultado != NULL && mensaje != NULL && mensajeError != NULL && minimo <= maximo && reintentos >= 0)
+	{
+		do
+		{
+			printf("%s",mensaje);
+			if(getInt(&buffer) == 0 && buffer >= minimo && buffer <= maximo)
+			{
+				*pResultado = buffer;
+				retorno = 0;
+				break;
+			}
+			reintentos--;
+			printf("%s",mensajeError);
+		}while(reintentos >= 0);
+	}
 	return retorno;
 }
 
